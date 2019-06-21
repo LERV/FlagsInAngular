3f6970e096235d9a5ee6e69740cdd31ab14e6648
Edward Rodriguez <lrodriguez@konradgroup.com>
2019-06-21 08:59:53 -0600

    Flags icon library added and use with ng-select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'flags-angular';
+  flags = [
+    { id: 1, country: 'China', code: 'cn' },
+    { id: 2, country: 'Germany', code: 'de' },
+    { id: 3, country: 'Uruguay', code: 'uy' },
+    { id: 4, country: 'France', code: 'fr' },
+    { id: 5, country: 'Japan', code: 'jp' },
+    { id: 6, country: 'Argentina', code: 'ar' },
+    { id: 7, country: 'Costa Rica', code: 'cr' }]
+  title = 'flags-angular 1';
+
+  selectedCountryId = 7;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
